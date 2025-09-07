9b0058588aabf6ef84c1838dcbc4ba01dc82d288
upskillingcoding-Ritik <upskillingcoding@gmail.com>
2025-09-07 10:03:04 +0530

    create hero and adwards section

--- src/landing_page/home/Adwards.js
+++ src/landing_page/home/Adwards.js
@@ -3,7 +3,35 @@ import React from 'react';
 
 function Adwards() {
     return (  
-        <h1>Adwards</h1>
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-6 col-md-6 col-sm-12 p-5">
+                    <img src="media/largestBroker.svg" alt="" />
+                </div>
+                <div className="col-lg-6 col-md-6 col-sm-12 p-5 mt-3">
+                    <h1>Largest stock broker in India</h1>
+                    <p className='mb-5'>2+ million Zerodha clients contribute to over 15% of all retail order volume in India daily by trading and investing in:</p>
+                    <div className="row">
+                        <div className="col-6">
+                            <ul>
+                                <li><p>Future and Options</p></li>
+                                <li><p>Commodity derivatives</p></li>
+                                <li><p>Currency derivatives</p></li>
+                            </ul>
+                        </div>
+                        <div className="col-6">
+                            <ul>
+                                <li><p>Stock & IPOs</p></li>
+                                <li><p>Direct mutual funds</p></li>
+                                <li><p>Bonds and Govy. Security</p></li>
+                            </ul>
+                        </div>
+                        <img src="media/pressLogos.png" alt="" style={{width: "80%"}}/>
+                    </div>
+                   
+                </div>
+            </div>
+        </div>
     );
 }
 
--- src/landing_page/home/Hero.js
+++ src/landing_page/home/Hero.js
@@ -3,7 +3,14 @@ import React from 'react';
 function HeroSection(){
     return (
         <>
-            <h1>HeroSection</h1>
+            <div className="container p-5 mb-5">
+                <div className="row text-center">
+                    <img src="media/homeHero.png" alt="" className="mb-5"/>
+                    <h1 className="mt-5">Invest in everything</h1>
+                    <p>Online platform to invest in stock, derivatives, mutual funds, and more</p>
+                    <button className='p-2 btn btn-primary fs-5 mb-5' style={{width: '25%', margin: '0 auto'}}>Signup Now</button>
+                </div>
+            </div>
         </>
     );
 }

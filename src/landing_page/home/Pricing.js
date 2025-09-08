3a635f681c38e0948a912c63726accc1395259dd
upskillingcoding-Ritik <upskillingcoding@gmail.com>
2025-09-08 21:35:54 +0530

    create pricing n stats

--- src/landing_page/home/Pricing.js
+++ src/landing_page/home/Pricing.js
@@ -3,7 +3,28 @@ import React from 'react';
 function Pricing() {
     return ( 
         <>
-            <h1>Pricing</h1>
+            <div className="conatiner p-5">
+                <div className="row p-5">
+                    <div className="col-4 p-5">
+                        <h1>Unbeatable Pricing</h1>
+                        <p>We pioneered the concept of discount broking and price transparency in India. Flat fees and no hidden charges.</p>
+                        <a href="" style={{textDecoration: "none"}}>See Pricing</a>
+                    </div>
+                    <div className="col-2"></div>
+                    <div className="col-6">
+                        <div className="row text-center p-5">
+                            <div className="col p-3 border">
+                                <h1 className='mb-3'>₹0</h1>
+                                <p>Free equity delivery and <br /> direct mutual funds</p>
+                            </div>
+                            <div className="col p-3 border">
+                                <h1 className='mb-3'>₹0</h1>
+                                <p>Intrady and F&O</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
      );
 }
--- src/landing_page/home/Stats.js
+++ src/landing_page/home/Stats.js
@@ -3,7 +3,31 @@ import React from 'react';
 function Stats(){
     return(
         <>
-            <h1>Stats</h1>
+            <div className="container p-5">
+                <div className="row p-5">
+                    <div className="col-lg-6 col-6 col-md-6 col-sm-12 p-5">
+                        <h1 className='fs-2 mb-5'>Trust with confidence</h1>
+                        <h2 className='fs-4'>Customer-first always</h2>
+                        <p className='text-muted'>That's why 1.3+ crore customers trust Zerodha with Rs3.5+ lakh crore worth of equity investment</p>
+
+                        <h2 className='fs-4'>No spam or gimmicks</h2>
+                        <p className='text-muted'>No gimmicks, spam, "gimmicks", or annoying push notifications. High quality apps that you use at your pace, the way you like</p>
+
+                        <h2 className='fs-4'>The Zerodha Universe</h2>
+                        <p className='text-muted'>Not just an app, but a whole ecosystem. Our investments in 30+ fintech startups offer you tailored services specific to you needs.</p>
+
+                        <h2 className='fs-4'>Do better with money</h2>
+                        <p className='text-muted'>With initiatives like Nudge and Kill Switch, we don't just facilites transaction, but actively help you do better with your money</p>
+                    </div>
+                    <div className="col-lg-6 col-6 col-md-6 col-sm-12 p-5">
+                        <img src="media/ecosystem.png" alt="" style={{width: '90%'}}/>
+                        <div className='text-center'>
+                            <a href="" className='mx-5' style={{textDecoration: "none"}}>Explore our Products</a>
+                            <a href="" style={{textDecoration: "none"}}>Try Kite demo</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }

9bae2c872e70ce0ae065f2554e28324d615f7042
upskillingcoding-Ritik <upskillingcoding@gmail.com>
2025-09-12 22:00:34 +0530

    Routing setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import HomePage from './landing_page/home/HomePage';
 import AboutPage from './landing_page/about/AboutPage';
 import PricingPage from './landing_page/pricing/PricingPage';
@@ -13,16 +14,26 @@ import Signup from './landing_page/signup/Signup';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    {/* <div> */}
-     <HomePage />
-     <AboutPage/>
-     <PricingPage />
-     {/* <products /> */}
-      <Products /> 
-      <SupportPage />
-      <Signup />
-    {/* </div> */}
-  </React.StrictMode>
+  // <React.StrictMode>
+  //    <HomePage />
+  //    <AboutPage/>
+  //    <PricingPage />
+  //     <Products /> 
+  //     <SupportPage />
+  //     <Signup />
+  // </React.StrictMode>
+
+
+  <BrowserRouter>
+    <Routes>
+        <Route path='/' element={<HomePage />}></Route>
+        <Route path='/signup' element={<Signup />}></Route>
+        <Route path='/about' element={<AboutPage />}></Route>
+        <Route path='/pricing' element={<PricingPage />}></Route>
+        <Route path='/products' element={<Products />}></Route>
+        <Route path='/support' element={<SupportPage />}></Route>
+    </Routes>
+  </BrowserRouter>
+
 );
 
